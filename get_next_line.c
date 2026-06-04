008f25e057417b97f29cb549415bad4ee0b47074
miguelcobweb <miguelcarvalhopt9@gmail.com>
2026-06-04 15:14:43 +0100

    Add files via upload

---
+++ get_next_line.c
@@ -0,0 +1,67 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: micampos <micampos@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/06 14:12:00 by micampos          #+#    #+#             */
+/*   Updated: 2026/06/04 14:05:40 by micampos         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*get_next_line(int fd)
+{
+	static char	buffer[BUFFER_SIZE + 1];
+	char		*helper;
+	char		*new_line;
+	int			received;
+
+	if (BUFFER_SIZE <= 0 || BUFFER_SIZE > 10000000)
+		return (NULL);
+	received = 1;
+	helper = NULL;
+	while (normal_strchr(buffer) == NULL && received > 0)
+	{
+		helper = normal_strjoin(helper, buffer);
+		received = read(fd, buffer, BUFFER_SIZE);
+		if (received < 0)
+			return (free(helper), NULL);
+		buffer[received] = '\0';
+	}
+	if (received == 0)
+	{
+		new_line = helper;
+		return (new_line);
+	}
+	return (new_line = extract_line(helper, buffer));
+}
+#include <stdio.h>
+int main(void)
+{
+	int fd;
+	char *str;
+
+	fd = open("text.txt", O_RDONLY);
+	// while ((str = get_next_line(0)) != NULL)  
+	// {
+	// 	printf("line: %s", str);
+	// 	printf("---------------------------\n");
+	// 	free(str);
+	// }
+	str = get_next_line(fd);
+	printf("line: %s", str);
+	free(str);
+	return (0);
+}
+
+
+read
+
+new line = hello\n
+char *stash = how 
+get_next_line 
+print 
+free
